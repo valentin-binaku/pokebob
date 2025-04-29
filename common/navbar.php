fceeae30911990369c9b87280c4f9d413410f29e
YenisLass <yenislassassi@gmail.com>
2025-04-29 20:25:16 +0200

    Update navbar.php
    "Accueil" au lieu de "Acceuil"

--- common/navbar.php
+++ common/navbar.php
@@ -14,7 +14,7 @@
 <nav class="navbar">
     <ul>
         <h3>Pokebob</h3>
-        <li><a href="../pages/home.php">Acceuil</a></li>
+        <li><a href="../pages/home.php">Accueil</a></li>
         <li><a href="../pages/open.php">Ouverture de carte</a></li>
         <li><a href="../pages/inventory.php">Inventaire</a></li>
         <li><a href="../pages/market.php">Marché</a></li>
