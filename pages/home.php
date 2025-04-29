8bea0a6fd56903ba92cb07eaaf35d0e0aed8a360
YenisLass <yenislassassi@gmail.com>
2025-04-29 17:05:49 +0200

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -1,14 +1,36 @@
-<?php 
-include "../common/config.php";
-include "../common/head.php";
-include "../common/navbar.php";
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <?php 
+    include "../common/config.php";
+    include "../common/head.php";
+    include "../common/navbar.php";
+    ?>
 
+    <h1>Bienvenue sur</h1>
+    <h1>POKEBOB</h1>
 
+    <p>(Logo du site)</p>
 
+    <h1>Principe du jeu</h1>
+    <p>Votre objectif est de collecter le plus de Bob possible ! Les Bob sont des être comparable à des pions, il existe plusieurs variantes de ses Bob toutes collectionnable en ouvrant des boosters.</p>
 
+    <h2>Les boosters</h2>
+    <p>Les boosters sont donnés jusqu'à une fois par heure. Chaque Bob possède ses propres stats : PV et ATQ. Tentez d'obtenir les meilleurs Bob possible !</p>
 
-<?php
-include "../common/footer.php";
-?>
+    <h2>Rareté des Bob</h2>
+    <p>Il existe également différentes rareté de Bob : commun, uncommun, rare et légendaire. Plus votre Bob est rare plus il prend de la valeur.</p>
+
+    <h2>Marché</h2>
+    <p>Vous pourrez par la suite mettre vos cartes sur le marché pour un certain prix. Les cartes que les autres joueurs ont mise en vente nseront également accessible et vous pourrez les acheter grâce à des BobCoin (BC)</p>
+
+    <?php
+    include "../common/footer.php";
+    ?>
+</body>
 </html>
