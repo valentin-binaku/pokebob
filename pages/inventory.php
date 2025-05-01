435775eb8feb232f3e1b5075f1faddfb9b742ea5
YenisLass <yenislassassi@gmail.com>
2025-05-01 17:31:31 +0200

    Update inventory.php

--- pages/inventory.php
+++ pages/inventory.php
@@ -1,7 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory</title>
+</head>
+<body>
 <?php 
 include "../common/config.php";
 include "../common/permission.php";
 include "../common/head.php";
 include "../common/navbar.php";
+
+try {
+    $nom = $_SESSION['utilNom'];
+    $userId = $_SESSION['user_id'];
+
+    $stmt = $pdo->prepare("
+        SELECT c.carteNom, c.carteRareté, c.image, i.Stock
+        FROM inventaire i
+        INNER JOIN carte c ON i.carteID = c.carteID
+        WHERE i.utilID = :userId
+    ");
+    $stmt->execute(['userId' => $userId]);
+    $cartes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Affichage des cartes
+    echo "<div class='grid'>";
+    foreach ($cartes as $carte) {
+        echo "<article>";
+        echo "<img src='public/image/cartes/{$carte['image']}' alt='{$carte['carteNom']}' style='height:200px;'>";
+        echo "<p>{$carte['carteNom']}<br><small>{$carte['carteRareté']}</small></p>";
+        echo "<p>x {$carte['Stock']}</p>";
+        echo "</article>";
+    }
+    echo "</div>";
+
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
+
+<?php
 include "../common/footer.php";
 ?>
+</body>
+</html>
