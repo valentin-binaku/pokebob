579e82968813d0d29d8c1a3ed46ab42205f25397
valentin-binaku <v.binaku05@gmail.com>
2025-05-04 22:49:58 +0200

    [add] systeme vente rapide

--- pages/inventory.php
+++ pages/inventory.php
@@ -17,22 +17,36 @@ try {
     $userId = $_SESSION['user_id'];
 
     $stmt = $pdo->prepare("
-        SELECT c.carteNom, c.carteRareté, c.carteDescription, c.image, i.Stock
-        FROM inventaire i
-        INNER JOIN carte c ON i.carteID = c.carteID
-        WHERE i.utilID = :userId
+    SELECT c.carteID, c.carteNom, c.carteRareté, c.carteDescription, c.image, i.Stock
+    FROM inventaire i
+    INNER JOIN carte c ON i.carteID = c.carteID
+    WHERE i.utilID = :userId
     ");
     $stmt->execute(['userId' => $userId]);
     $cartes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     // Affichage des cartes
     echo "<div class='grid'>";
+
+
     foreach ($cartes as $carte) {
+        $gain = match($carte['carteRareté']) {
+            'common' => 5,
+            'uncommun' => 20,
+            'rare' => 50,
+            'legendary' => 250,
+            default => 0
+        };
         echo "<article>";
         echo "<img src='../public/image/{$carte['image']}' alt='{$carte['carteNom']}' style='height:200px;'>";
         echo "<p>{$carte['carteNom']}<br><small>{$carte['carteRareté']}</small></p>";
         echo "<p><small>{$carte['carteDescription']}</small></p>";
         echo "<p>x {$carte['Stock']}</p>";
+        echo "<form action='quick_sell.php' method='POST'>";
+        echo "<input type='hidden' name='carte_id' value='{$carte['carteID']}'>";
+        echo "<input type='hidden' name='gain' value='{$gain}'>";
+        echo "<button type='submit'>Vente rapide</button>";
+        echo "</form>";        
         echo "</article>";
     }
     echo "</div>";
---
+++ pages/quick_sell.php
@@ -0,0 +1,41 @@
+<?php
+include "../common/config.php";
+include "../common/permission.php";
+include "../common/head.php";
+include "../common/navbar.php";
+
+$carteID = $_POST['carte_id'];
+$gain = $_POST['gain'];
+$userID = $_SESSION['user_id'];
+
+$query_stock = "SELECT Stock FROM inventaire WHERE carteID = :carteID AND utilID = :userID";
+$stmt_stock = $pdo->prepare($query_stock);
+$stmt_stock->bindParam(':carteID', $carteID);
+$stmt_stock->bindParam(':userID', $userID);
+$stmt_stock->execute();
+$stock = $stmt_stock->fetchColumn();
+
+if ($stock > 1) {
+    $query_update_stock = "UPDATE inventaire SET Stock = Stock - 1 WHERE carteID = :carteID AND utilID = :userID";
+    $stmt_update_stock = $pdo->prepare($query_update_stock);
+    $stmt_update_stock->bindParam(':carteID', $carteID);
+    $stmt_update_stock->bindParam(':userID', $userID);
+    $stmt_update_stock->execute();
+} else {
+    $query_delete = "DELETE FROM inventaire WHERE carteID = :carteID AND utilID = :userID";
+    $stmt_delete = $pdo->prepare($query_delete);
+    $stmt_delete->bindParam(':carteID', $carteID);
+    $stmt_delete->bindParam(':userID', $userID);
+    $stmt_delete->execute();
+}
+
+// Ajouter les bobcoins à l'utilisateur
+$query2 = "UPDATE utilisateur SET utilMoney = utilMoney + :gain WHERE utilID = :userID";
+$stmt2 = $pdo->prepare($query2);
+$stmt2->bindParam(':gain', $gain);
+$stmt2->bindParam(':userID', $userID);
+$stmt2->execute();
+
+header('Location: inventaire.php');
+exit();
+?>
